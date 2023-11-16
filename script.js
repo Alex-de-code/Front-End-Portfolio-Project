19db54622f792025ac717997b036bdc2c61dc8d7
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-11-16 12:42:55 -0500

    created an event listener for when a user selected a category. Whatever category is selected logs to the console

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // wiring html with event listeners 
 
-
 // event listener for word of the day 
 const wordOfTheDay = document.querySelector(".word-of-day");
 wordOfTheDay.addEventListener("click", () => {
     //logs to console in dev web tools
     console.log("You have just clicked the word of the day button!"); 
 });
-
 
 //event listener for the search bar 
 const searchBar = document.querySelector(".search-bar");
@@ -18,8 +16,25 @@ const submitButton = document.querySelector("[type=submit]");
 submitButton.addEventListener("click", (event) => {
     //this is to prevent page from refreshing while testing search button
     event.preventDefault();
-    console.log("You have just clicked the search button!")
+    console.log("You have just clicked the search button!");
 });
+
+
+// creating a variable that stores all elements with the class "dropdown-item"
+const dropDownItems = document.querySelectorAll(".dropdown-item");
+// iterate over each drop down item 
+dropDownItems.forEach(item => {
+    //add a click event listener to each dropdown item 
+    item.addEventListener("click", function(event) {
+        // prevent default behavior of the click event 
+        event.preventDefault();
+        // hry value of the name attribute of clicked item
+        const selectedCategory = event.target.getAttribute("name");
+        // log the selected category to the console 
+        console.log(`You've selected the ${selectedCategory} category!`); 
+    })
+})
+
 
 // const form = document.querySelector("form");
 // form.addEventListener("submit", (event) => {

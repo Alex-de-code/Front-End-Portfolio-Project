370b6618c188c77b9d59b18b73c0147dc595d043
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-11-16 11:57:08 -0500

    added an event listener for the submit/search bttn. it only logs to the dev console for the time being while testing site. will change later.

--- script.js
+++ script.js
@@ -4,5 +4,27 @@
 // event listener for word of the day 
 const wordOfTheDay = document.querySelector(".word-of-day");
 wordOfTheDay.addEventListener("click", () => {
+    //logs to console in dev web tools
     console.log("You have just clicked the word of the day button!"); 
 });
+
+
+//event listener for the search bar 
+const searchBar = document.querySelector(".search-bar");
+console.log(searchBar.value);  
+
+//event listener for search button 
+const submitButton = document.querySelector("[type=submit]");
+submitButton.addEventListener("click", (event) => {
+    //this is to prevent page from refreshing while testing search button
+    event.preventDefault();
+    console.log("You have just clicked the search button!")
+});
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     console.log(event.target.searchBar.value);
+
+// })
